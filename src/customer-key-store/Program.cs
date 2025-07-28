13cb241bab132a43c890a77dc878db5f0cf40a45
chenkevin <chenkevin@synology.com>
2025-07-28 11:28:17 +0800

    Fixup listening port and dockerfile

--- src/customer-key-store/Program.cs
+++ src/customer-key-store/Program.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 namespace CustomerKeyStore
 {
+    using System;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Logging;
@@ -12,14 +13,18 @@ namespace CustomerKeyStore
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+            return WebHost.CreateDefaultBuilder(args)
             .UseStartup<Startup>()
+            .UseUrls($"http://0.0.0.0:{port}")
             .ConfigureLogging((context, logging) =>
             {
                 logging.AddEventLog(eventLogSettings =>
                 {
                 });
             });
+        }
     }
 }
